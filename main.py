7e3bd75eb527d55179c3640686bdc600e235c666
Andrew McIntyre <andrewmcintyre365@gmail.com>
2021-03-30 13:57:00 -0700

    Performs basic search on a website, add exit wrapper function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from pyvirtualdisplay import Display
-import xvfbwrapper
-#from webdriver_manager.chrome import ChromeDriverManager
+
+def _exit(driver, display):
+    """Exit wrapper function.
+    Ensures that the web driver exits cleanly, and cleans up the chrome driver and Xvfb display
+    to prevent rogue processes. 
+    """
+    driver.quit()
+    display.stop()
 
 # Initialize virtual display, needed in order to start chromium-browser
 display = Display(visible=0, size=(800, 600))
@@ -10,16 +21,29 @@ display.start()
 # Set arguments to use for browser 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
-options.add_argument('--test-type')
-options.add_argument("--headless")
+#options.add_argument('--test-type')
+#options.add_argument("--headless")
 #options.binary_location = "/usr/lib/chromium-browser/chromedriver"
 options.binary_location = "/usr/bin/chromium-browser"
 # Create driver
 driver = webdriver.Chrome(options=options)
 
-# Open a website
-driver.get('http://example.com/')
-print(driver.page_source.encode("utf-8"))
-driver.quit()
+# Setup complete
 
-testvar2 = 0
+# Open a website
+#driver.get('https://www.instagram.com/')
+driver.get('https://store.gamersnexus.net/')
+#WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "KPnG0")))
+try:
+    search_box = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/div/div/div/div[2]/div/div/div/input')
+    search_box.send_keys("mouse")
+    search_box.send_keys(Keys.RETURN)
+    print(driver.page_source.encode("utf-8"))
+    #print(driver)
+except Exception as e:
+    print(e)
+    _exit(driver, display)
+#driver.find_element_by_id('loginForm')
+#print(driver.page_source.encode("utf-8"))
+
+_exit(driver, display)
